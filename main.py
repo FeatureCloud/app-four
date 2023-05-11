bc3d77271ed06af078552643db60e4a0918e18cf
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-05-11 13:58:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from bottle import Bottle
+
+from FeatureCloud.app.api.http_ctrl import api_server
+from FeatureCloud.app.api.http_web import web_server
+
+from FeatureCloud.app.engine.app import app
+import os
+import states
+
+server = Bottle()
+
+
+def is_native():
+    path_prefix = os.getenv("PATH_PREFIX")
+    if path_prefix:
+        return False
+    return True
+
+
+def run_app():
+    server.mount('/api', api_server)
+    server.mount('/web', web_server)
+    server.run(host='localhost', port=5000)
+
+
+if __name__ == '__main__':
+    app.register()
+    if is_native():
+        app.handle_setup(client_id='1', coordinator=True, clients=['1'])
+    else:
+        run_app()
