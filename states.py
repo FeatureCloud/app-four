60d9c456ef9831fea86f8a2fc570b395ad4b29c9
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-06-05 16:47:16 +0200

    initial commit

--- states.py
+++ states.py
@@ -1,85 +1,89 @@
-"""
-Tips:
-1. Do not rename the states!
-2. Return values of 'run' method will be discarded!
-3. Do not define any arguments for run method!
-4. Do not rename predefined attributes in classes
-5. Do not use predefined attributes in classes for unrelated purposes
-"""
-
-INPUT_DIR = "/mnt/input"
-OUTPUT_DIR = "/mnt/output"
-CONFIG_FILE = "/mnt/input/config.yml"
+from FeatureCloud.app.engine.app import AppState, app_state, Role
+import myapp
+import utils
 
 
-class InitialState:
-    """
-    Attributes:
-        data_to_broadcast: list
-            Values or messages from coordinator to all client
-    """
+app_instance = myapp.MyApp()
 
-    def __init__(self):
-        self.data_to_broadcast = None
+if utils.is_centralized(myapp.APP_NAME):
+    if utils.file_has_class('myapp.py', 'Centralized'):
+        @app_state(name='initial', role=Role.BOTH)
+        class InitialState(AppState):
 
-    def run(self):
-        pass
+            def register(self):
+                self.register_transition('Centralized')
 
-
-class LocalTraining:
-    """
-    Attributes:
-        received_data: list
-            Values or messages from coordinator to all client
-        data_to_send: list
-            Local parameters to share with the coordinator
-        last_round: bool
-            if True, the training loop is over, i.e., go to WriteResults!
-        use_smpc: bool
-            Use SMPC if True
-
-    """
-
-    def __init__(self):
-        self.received_data = None
-        self.data_to_send = None
-        self.use_smpc = False
-        self.last_round = None
-
-    def run(self):
-        pass
+            def run(self):
+                return 'Centralized'
 
 
-class GlobalAggregation:
-    """
-    Attributes:
-        local_data: list
-            received local parameters
-        use_smpc: bool
-            If True, local_data contains aggregated data from SMPC
-            If False, local_data contains all clients' data
-        data_to_broadcast: list
-            Values or messages from coordinator to all client
+        @app_state('Centralized')
+        class Centralized(AppState):
 
-    """
+            def register(self):
+                self.register_transition('terminal')
 
-    def __init__(self):
-        self.local_data = None
-        self.use_smpc = False
-        self.data_to_broadcast = None
+            def run(self):
+                app_instance.centralized()
+                return 'terminal'
+else:
+    @app_state(name='initial', role=Role.BOTH)
+    class InitialState(AppState):
 
-    def run(self):
-        pass
+        def register(self):
+            self.register_transition('Local_Training')
+
+        def run(self):
+            data_to_broadcast = app_instance.load_data()
+            self.broadcast_data(data_to_broadcast)
+            return 'Local_Training'
 
 
-class WriteResults:
-    """
-    Attributes:
+    @app_state('Local_Training')
+    class LocalTraining(AppState):
 
-    """
+        def register(self):
+            self.register_transition('Global_Aggregation', role=Role.COORDINATOR)
+            self.register_transition('Local_Training', role=Role.PARTICIPANT)
+            self.register_transition('Write_Results', role=Role.BOTH)
 
-    def __init__(self):
-        pass
+        def run(self):
+            received_data = self.await_data()
+            data_to_send = app_instance.local_training(global_parameters=received_data)
+            # if self.data_to_send is None:
+            #     raise NotImplementedError("Local_Training is expected to have some local parameters to share")
+            self.send_data_to_coordinator(data_to_send, use_smpc=app_instance.config["use_smpc"])
+            if self.last_round:
+                return "Write_Results"
+            if self.is_coordinator:
+                return "Global_Aggregation"
+            return "Local_Training"
 
-    def run(self):
-        pass
+
+    @app_state('Global_Aggregation')
+    class GlobalAggregation(AppState):
+
+        def register(self):
+            self.register_transition('Local_Training', role=Role.COORDINATOR)
+
+        def run(self):
+            if self.use_smpc:
+                local_data = self.aggregate_data(use_smpc=True)
+            else:
+                local_data = self.gather_data()
+            data_to_broadcast = app_instance.global_aggregation(local_parameters=local_data)
+            # if self.data_to_broadcast is None:
+            #     raise NotImplementedError("Global_Aggregation is expected to have some local parameters to share")
+            self.broadcast_data(data_to_broadcast)
+            return 'Local_Training'
+
+
+    @app_state('Write_Results')
+    class WriteResults(AppState):
+
+        def register(self):
+            self.register_transition('terminal')
+
+        def run(self):
+            app_instance.write_results()
+            return 'terminal'
