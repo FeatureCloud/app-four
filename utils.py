25bdc0edcc1825bc812dbe1492703cb51e9afc64
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2023-06-20 14:44:55 +0200

    code cleaned

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 """
-    FeatureCloud Four States App  Template
+    FeatureCloud Four States App Template
     Copyright 2023 Mohammad Bakhtiari. All Rights Reserved.
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
